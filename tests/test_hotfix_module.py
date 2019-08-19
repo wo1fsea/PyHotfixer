4c595dc9a7637dd9888989d862791fcf71dd00a4
wo1fsea <wo1fsea@126.com>
2019-08-19 21:58:20 +0800

    add: travis.yml

---
+++ tests/test_hotfix_module.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""----------------------------------------------------------------------------
+Author:
+    Huang Quanyong
+    gzhuangquanyong@corp.netease.com
+Date:
+    2019/8/19
+Description:
+    test_hotfix_module.py
+----------------------------------------------------------------------------"""
+
+import unittest
+
+
+class HotfixModuleTestCase(unittest.TestCase):
+
+	def test_hotfix_module(self):
+        pass
